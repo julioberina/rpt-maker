79a82aa2f7bb7815f6b2751e0786c0b2e16cb9a8
Julio Berina <julioberina@gmail.com>
2021-06-23 18:05:40 -0700

    Implement signup component form with styles

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  private emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
+  public fg: any;
+  public inputType = 'password';
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.fg = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.pattern(this.emailRegex)])],
+      password: ['', Validators.required]
+    });
   }
 
+  toggleVisibility() {
+    this.inputType = this.inputType == 'password' ? 'text' : 'password';
+  }
 }
